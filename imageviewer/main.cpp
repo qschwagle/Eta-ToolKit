a8a35af219864223a3df88660487366917a42b57
Quinton Schwagle <qschwagle@gmail.com>
2021-01-22 04:34:48 -0500

    added comments

--- imageviewer/main.cpp
+++ imageviewer/main.cpp
@@ -6,14 +6,27 @@
 
 int main(int argc, char** argv)
 {
+	// The Application itself
 	etk::Application app;
+
+	// create the window
 	int id = app.CreateWindow("Image Viewer");
 	auto window = app.GetWindow(id);
+
+	// set the background color
 	window->SetColor(etk::colors::PURPLE);
+
+	// create a scene
 	auto scene = std::make_shared<etk::Scene>(L"Main");
 	window->SetScene(scene);
+
+	// Add a button to the scene
 	auto button = std::make_shared<etk::Button>();
 	scene->SetWidget(L"SimpleButton", L"Main", button);
+
+	// initialize the app
 	app.Init(argc, argv);
+
+	// run the app
 	return app.Run();
 }
