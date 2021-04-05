c982a9b253867a4721619fc2198c92b7115b58af
Quinton Schwagle <qschwagle@gmail.com>
2021-04-05 18:24:02 -0400

    Work on changing image sizes.

--- etk/renderer/opengl/gl_image.h
+++ etk/renderer/opengl/gl_image.h
@@ -14,7 +14,17 @@ public:
 	void Draw(std::weak_ptr<ScreenBox> box) override;
 
 	void LoadImage(unsigned char* data, int width, int height, int channels) override;
+
+	void SetWidth(int w) override {
+
+	}
+
+	void SetHeight(int h) override {
+
+	}
 private:
+	int mExWidth{ -1 };
+	int mExHeight{ -1 };
 	int mWidth{ -1 };
 	int mHeight{ -1 };
 	int mChannels{ -1 };
